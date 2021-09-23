1424d2d44081a996e83c4bc9ac54e92a9d4b7f09
Alexander M <contactimedici@gmail.com>
2021-09-23 12:23:32 -0400

    conflict free

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
-<<<<<<< HEAD
 import FormInput from './Checkout/FormInput';
 
 const AddressForm = () => {
@@ -17,35 +16,8 @@ const AddressForm = () => {
                 </form>
             </FormProvider>
          
-=======
-import  FormInput  from './CustomTextField';
-
-
-const AddressForm = () => {
-    const methods = useForm();
-
-    return (
-        <>
-            <Typography variant="h6" gutterBottom>Shipping Address Form </Typography>
-            <FormProvider {...methods}>
-                <form onSubmit=' ' >
-                    <Grid container spacing={3}>
-                        <FormInput required name="firstName" label="First name" />
-                        <FormInput required name="lastName" label="Last name" />
-                        <FormInput required name="address1" label="Address line 1" />
-                        <FormInput required name="email" label="Email" />
-                        <FormInput required name="city" label="City" />
-                        <FormInput required name="zip" label="Zip / Postal code" />
-                    </Grid>
-                </form>
-            </FormProvider>
->>>>>>> fe89c9d58235ee4ee5d6dda21a85795ffb26d52e
         </>
     );
 }
 
-<<<<<<< HEAD
-export default AddressForm
-=======
 export default AddressForm;
->>>>>>> fe89c9d58235ee4ee5d6dda21a85795ffb26d52e
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -3,33 +3,19 @@ import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider,
 import useStyles from './styles';
 import AddressForm from "../AddressForm";
 import PaymentForm from '../PaymentForm';
-<<<<<<< HEAD
-=======
-
->>>>>>> fe89c9d58235ee4ee5d6dda21a85795ffb26d52e
 
 const steps = ["Shipping address", "Payment details"];
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
-<<<<<<< HEAD
-     const Confirmation = () => (
-=======
     const Confirmation = () => (
->>>>>>> fe89c9d58235ee4ee5d6dda21a85795ffb26d52e
         <div>
             confirmation
         </div>
     );
-<<<<<<< HEAD
-    const Form = () => activeStep === 0
-        ? <AddressForm />
-        :<PaymentForm/>
-=======
     const Form = ( ) => (activeStep === 0
         ? <AddressForm  />
         : <PaymentForm  />);
->>>>>>> fe89c9d58235ee4ee5d6dda21a85795ffb26d52e
     return (
         <>
             <div className={classes.toolbar} />
@@ -43,11 +29,7 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
-<<<<<<< HEAD
-                    { activeStep === steps.length  ? <Confirmation /> :<Form/>}
-=======
                     {activeStep === steps.length ? <Confirmation /> : <Form />}
->>>>>>> fe89c9d58235ee4ee5d6dda21a85795ffb26d52e
                 </Paper>
             </main>
         </>
