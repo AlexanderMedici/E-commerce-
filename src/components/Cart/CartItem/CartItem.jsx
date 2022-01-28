4ad00191607f3a53d0ebd359871cad554f362042
Alexander M <contactimedici@gmail.com>
2022-01-27 19:20:09 -0500

    update

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 
 import useStyles from './styles';
 
-const CartItem = ({ item: lineItemId, onUpdateCartQty, onRemoveFromCart }) => {
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
   const classes = useStyles();
-    const lineItemIds = lineItemId;
-  const handleUpdateCartQty = (lineItemIds, newQuantity) => onUpdateCartQty(lineItemIds, newQuantity);
 
-  const handleRemoveFromCart = (lineItemIds) => onRemoveFromCart(lineItemIds);
+  const handleUpdateCartQty = (lineItemId, newQuantity) => onUpdateCartQty(lineItemId, newQuantity);
+
+  const handleRemoveFromCart = (lineItemId) => onRemoveFromCart(lineItemId);
 
   return (
     <Card className="cart-item">
-      <CardMedia image={lineItemIds.media.source} alt={lineItemIds.name} className={classes.media} />
-      <CardContent style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="h4">{lineItemIds.name}</Typography>
-        <Typography variant="h5">{lineItemIds.line_total.formatted_with_symbol}</Typography>
+      <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
       </CardContent>
-      <CardActions style={{ justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Button type="button" size="small" onClick={() => handleUpdateCartQty(lineItemIds.id, lineItemIds.quantity - 1)}>-</Button>
-          <Typography>&nbsp;{lineItemIds.quantity}&nbsp;</Typography>
-          <Button type="button" size="small" onClick={() => handleUpdateCartQty(lineItemIds.id, lineItemIds.quantity + 1)}>+</Button>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
+          <Typography>&nbsp;{item.quantity}&nbsp;</Typography>
+          <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
         </div>
-        <Button variant="contained" type="button" color="secondary" onClick={() => handleRemoveFromCart(lineItemIds.id)}>Remove</Button>
+        <Button variant="contained" type="button" color="secondary" onClick={() => handleRemoveFromCart(item.id)}>Remove</Button>
       </CardActions>
     </Card>
   );
